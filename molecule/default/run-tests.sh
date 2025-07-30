10b4d3295f9f0f650033c424c392bb21f1f1e969
Dmitry Sukhodoev <raven428@gmail.com>
2025-07-30 11:44:47 +0500

    refactor active tests

--- molecule/default/run-tests.sh
+++ molecule/default/run-tests.sh
@@ -12,38 +12,39 @@ printf "\n\n\nmolecule [create] action\n"
 ANSIBLE_LOG_PATH="${LOG_PATH}-0create" \
   ansible-docker.sh molecule -v create -s "${sce}"
 
-for prop_mode in start stop mod_start mod_fail; do
+for prop_mode in start stop mod_start start_fail mod_fail start_run_fail mod_run_fail; do
   printf "\n\n\nmolecule [%s] mode [converge] check\n" "${prop_mode}"
-  ANSIBLE_LOG_PATH="${LOG_PATH}-1check" \
+  ANSIBLE_LOG_PATH="${LOG_PATH}-${prop_mode}-1check" \
     ansible-docker.sh molecule -v converge -s "${sce}" -- --check \
     --extra-vars prop_mode="${prop_mode}"
 
   printf "\n\n\nmolecule [converge] action\n"
-  ANSIBLE_LOG_PATH="${LOG_PATH}-2converge" \
+  ANSIBLE_LOG_PATH="${LOG_PATH}-${prop_mode}-2converge" \
     ansible-docker.sh molecule -v converge -s "${sce}" -- \
     --extra-vars prop_mode="${prop_mode}"
 
   printf "\n\n\nmolecule [idempotence] action\n"
-  ANSIBLE_LOG_PATH="${LOG_PATH}-3converge" \
+  ANSIBLE_LOG_PATH="${LOG_PATH}-${prop_mode}-3idempotence" \
     ansible-docker.sh molecule -v idempotence -s "${sce}" -- \
     --extra-vars prop_mode="${prop_mode}"
 
   printf "\n\n\nmolecule [converge] check\n"
-  ANSIBLE_LOG_PATH="${LOG_PATH}-4converge" \
+  ANSIBLE_LOG_PATH="${LOG_PATH}-${prop_mode}-4converge-check" \
     ansible-docker.sh molecule -v converge -s "${sce}" -- --check \
     --extra-vars prop_mode="${prop_mode}"
 
-  printf "\n\n\nmolecule [idempotence] check\n"
-  [[ "${prop_mode}" == 'mod_fail' ]] || {
-    ANSIBLE_LOG_PATH="${LOG_PATH}-5idempotence" \
+  [[ "${prop_mode}" == 'start' || "${prop_mode}" == 'stop' ||
+    "${prop_mode}" == 'mod_start' ]] && {
+    printf "\n\n\nmolecule [idempotence] check\n"
+    ANSIBLE_LOG_PATH="${LOG_PATH}-${prop_mode}-5idempotence-check" \
       ansible-docker.sh molecule -v idempotence -s "${sce}" -- --check \
       --extra-vars prop_mode="${prop_mode}"
   }
 done
 
 for prop_mode in stop_proc stop_port; do
-  printf "\n\n\nmolecule [converge] action\n"
-  ANSIBLE_LOG_PATH="${LOG_PATH}-2converge" \
+  printf "\n\n\nmolecule [%s] mode [converge]\n" "${prop_mode}"
+  ANSIBLE_LOG_PATH="${LOG_PATH}-${prop_mode}-1converge" \
     ansible-docker.sh molecule -v converge -s "${sce}" -- \
     --extra-vars prop_mode="${prop_mode}"
 done
