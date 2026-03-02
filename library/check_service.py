ebfeab25eb49a04834fb05fa7b809c9e4c39ce50
Dmitry Sukhodoev <raven428@gmail.com>
2026-03-02 16:08:55 +0000

    update linter happines

--- library/check_service.py
+++ library/check_service.py
@@ -99,7 +99,7 @@ import time
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.common.text.converters import to_native
 
-# pylint: disable=import-error,disable=no-name-in-module
+# pylint: disable=import-error,no-name-in-module
 from ansible.module_utils.mega_launch import (  # type: ignore[reportMissingImports]
   calc_ports,
 )
